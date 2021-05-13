7ec0748a43065d0128d4b7bc3e8d924478a2b608
jimao <1982775886@qq.com>
2021-05-13 16:08:34 +0800

    MetaCoin

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+const JimaoCoin = artifacts.require("JimaoCoin");
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.link(ConvertLib, JimaoCoin);
+  deployer.deploy(JimaoCoin);
 };
--- test/TestMetaCoin.sol
+++ test/TestMetaCoin.sol
@@ -2,24 +2,24 @@ pragma solidity >=0.4.25 <0.7.0;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
-import "../contracts/MetaCoin.sol";
+import "../contracts/JimaoCoin.sol";
 
 contract TestMetaCoin {
 
   function testInitialBalanceUsingDeployedContract() public {
-    MetaCoin meta = MetaCoin(DeployedAddresses.MetaCoin());
+    JimaoCoin meta = JimaoCoin(DeployedAddresses.JimaoCoin());
 
     uint expected = 10000;
 
-    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
+    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 JimaoCoin initially");
   }
 
   function testInitialBalanceWithNewMetaCoin() public {
-    MetaCoin meta = new MetaCoin();
+    JimaoCoin meta = new JimaoCoin();
 
     uint expected = 10000;
 
-    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
+    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 JimaoCoin initially");
   }
 
 }
