dd09f8d7adda4e61603d71ef626bd644d6d1a074
jimao <1982775886@qq.com>
2021-05-13 16:09:53 +0800

    metaCoin

--- test/metacoin.js
+++ test/metacoin.js
@@ -2,36 +2,36 @@ const JimaoCoin = artifacts.require("JimaoCoin");
 
 contract('JimaoCoin', (accounts) => {
   it('should put 10000 JimaoCoin in the first account', async () => {
-    const metaCoinInstance = await JimaoCoin.deployed();
-    const balance = await metaCoinInstance.getBalance.call(accounts[0]);
+    const jimaoCoinInstance = await JimaoCoin.deployed();
+    const balance = await jimaoCoinInstance.getBalance.call(accounts[0]);
 
     assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
   });
   it('should call a function that depends on a linked library', async () => {
-    const metaCoinInstance = await JimaoCoin.deployed();
-    const metaCoinBalance = (await metaCoinInstance.getBalance.call(accounts[0])).toNumber();
-    const metaCoinEthBalance = (await metaCoinInstance.getBalanceInEth.call(accounts[0])).toNumber();
+    const jimaoCoinInstance = await JimaoCoin.deployed();
+    const jimaoCoinBalance = (await jimaoCoinInstance.getBalance.call(accounts[0])).toNumber();
+    const jimaoCoinEthBalance = (await jimaoCoinInstance.getBalanceInEth.call(accounts[0])).toNumber();
 
-    assert.equal(metaCoinEthBalance, 2 * metaCoinBalance, 'Library function returned unexpected function, linkage may be broken');
+    assert.equal(jimaoCoinEthBalance, 2 * jimaoCoinBalance, 'Library function returned unexpected function, linkage may be broken');
   });
   it('should send coin correctly', async () => {
-    const metaCoinInstance = await JimaoCoin.deployed();
+    const jimaoCoinInstance = await JimaoCoin.deployed();
 
     // Setup 2 accounts.
     const accountOne = accounts[0];
     const accountTwo = accounts[1];
 
     // Get initial balances of first and second account.
-    const accountOneStartingBalance = (await metaCoinInstance.getBalance.call(accountOne)).toNumber();
-    const accountTwoStartingBalance = (await metaCoinInstance.getBalance.call(accountTwo)).toNumber();
+    const accountOneStartingBalance = (await jimaoCoinInstance.getBalance.call(accountOne)).toNumber();
+    const accountTwoStartingBalance = (await jimaoCoinInstance.getBalance.call(accountTwo)).toNumber();
 
     // Make transaction from first account to second.
     const amount = 10;
-    await metaCoinInstance.sendCoin(accountTwo, amount, { from: accountOne });
+    await jimaoCoinInstance.sendCoin(accountTwo, amount, { from: accountOne });
 
     // Get balances of first and second account after the transactions.
-    const accountOneEndingBalance = (await metaCoinInstance.getBalance.call(accountOne)).toNumber();
-    const accountTwoEndingBalance = (await metaCoinInstance.getBalance.call(accountTwo)).toNumber();
+    const accountOneEndingBalance = (await jimaoCoinInstance.getBalance.call(accountOne)).toNumber();
+    const accountTwoEndingBalance = (await jimaoCoinInstance.getBalance.call(accountTwo)).toNumber();
 
 
     assert.equal(accountOneEndingBalance, accountOneStartingBalance - amount, "Amount wasn't correctly taken from the sender");
